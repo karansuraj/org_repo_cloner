030b5ff5207d4218cf5542fddbc3f73971e7ac83
Karan Suraj <karansuraj@gmail.com>
2021-02-03 21:59:48 -1000

    fixes bugs and adds zipping functions

--- getRepos.js
+++ getRepos.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
-const fs = require('fs');
+const fs = require('fs-extra');
 const _ = require('lodash');
 const git = require('simple-git/promise');
 const { Octokit } = require("@octokit/rest");
+const archiver = require('archiver');
 
 
 const fullListFile = 'lists/fullList.json';
@@ -18,7 +19,7 @@ async function write(filename, data) {
     // console.log("Data written to " + filename + "!");
     return true;
   } catch(err) {
-    logger.error("Error writing to file '"+filename+"'! => "+err);
+    console.error("Error writing to file '"+filename+"'! => "+err);
     return false;
   }
 }
@@ -28,9 +29,22 @@ async function read(filename) {
     let rawData = fs.readFileSync(filename);
     return JSON.parse(rawData);
   } catch(err) {
-    logger.error("Error reading from file '"+filename+"'! => "+err);
+    console.error("Error reading from file '"+filename+"'! => "+err);
     return null;
   }
+}
+
+// Return a list of files from the directory 'srcPath'
+async function getDirFiles(srcPath) {
+  let files;
+  console.log('Reading directory: '+srcPath);
+  try {
+    files = fs.readdirSync(srcPath);
+  } catch(err) {
+    console.error('Failed to read directory '+srcPath+' ==> ', err);
+    throw err;
+  }
+  return files;
 }
 
 /* ---------------------- Git Repo Clone Function ---------------------- */
@@ -43,6 +57,54 @@ async function gitClone(gitURL, directory) {
     // throw err;
   });
 }
+
+
+/* ---------------------- Archiver Function ---------------------- */
+
+async function zipAFolder(sourceDirectory, zipDirectory, filename) {
+  // create a file to stream archive data to.
+  let zipFilename = zipDirectory+'/'+filename+'.zip'
+  const output = fs.createWriteStream(__dirname + zipFilename);
+  const archive = archiver('zip', {
+    zlib: { level: 9 } // Sets the compression level.
+  });
+
+  // listen for all archive data to be written
+  // 'close' event is fired only when a file descriptor is involved
+  output.on('close', function() {
+    console.log(zipFilename+' written to with '+archive.pointer() + ' total bytes');
+    // console.log('archiver has been finalized and the output file descriptor has closed.');
+  });
+
+  // good practice to catch warnings (ie stat failures and other non-blocking errors)
+  archive.on('warning', function(err) {
+    if (err.code === 'ENOENT') {
+      // log warning
+      console.error("No such file or directory!");
+    } else {
+      // throw error
+      console.error(err);
+      throw err;
+    }
+  });
+
+  // good practice to catch this error explicitly
+  archive.on('error', function(err) {
+    console.error(err);
+    throw err;
+  });
+
+  // pipe archive data to the file
+  archive.pipe(output);
+
+  // append files from a sub-directory, putting its contents at the root of archive
+  archive.directory(sourceDirectory+'/', false);
+
+  // finalize the archive (ie we are done appending files but streams have to finish yet)
+  // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
+  archive.finalize();
+}
+
 
 
 /* ---------------------- Git Octokit API Function ---------------------- */
@@ -84,7 +146,7 @@ async function getListOfRepos() {
 }
 
 
-/* ---------------------- Filter and Clone Repos ---------------------- */
+/* ---------------------- Filter Clone, and Zip Repos ---------------------- */
 
 async function filterList() {
   let repoList = await read(fullListFile);
@@ -120,13 +182,29 @@ async function cloneRepos() {
     // console.log(directory);
     await gitClone(item, directory);
   }
-
 }
+
+async function zipRepos() {
+  let repoDir = __dirname+'/repos';
+  // Get list of repos
+  let repos = await getDirFiles(repoDir);
+  // Loop repos, zipping each one up to the /zippedRepos folder
+  for(let repoName of repos) {
+    if(repoName !== '.placeholder') {
+      await zipAFolder(repoDir+'/'+repoName,'/zippedRepos',repoName);
+    }
+  }
+}
+
+
+
+
 
 async function getFilterNCloneRepos() {
   await getListOfRepos();
   await filterList();
   await cloneRepos();
+  await zipRepos();
 }
 
 getFilterNCloneRepos();
